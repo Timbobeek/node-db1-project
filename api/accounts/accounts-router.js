a9c055151bfaf1dc3a2a7565d3e2da4058f8a07c
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-14 21:53:10 -0600

    first getter works, needs middleware

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
 
+const Account = require('../accounts/accounts-model')
+
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log(req)
+  Account.getAll()
+    .then((accounts) =>{
+      if (!accounts){
+        res.status(200).json([]);
+      } else {
+        res.status(200).json(accounts);
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message: 'The accounts info could not be retrieved'
+      });
+    })
 })
 
 router.get('/:id', (req, res, next) => {
