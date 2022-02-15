2015245872d806197bbbd496229f5c085c136cf4
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-15 01:24:59 -0600

    middleware done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,53 @@
+const Account = require("./accounts-model");
+const db = require("../../data/db-config");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
-}
+  const err = { status: 400 };
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    err.message = "name and budget are required";
+  } else if (typeof name !== "string") {
+    err.message = "name and budget are required";
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    err.message = "name of account must be between 3 and 100";
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    err.message = "budget of account must be a number";
+  } else if (budget < 0 || budget > 1000000) {
+    err.message = "budget of account is too large or too small";
+  }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  if (err.message) {
+    next(err);
+  } else {
+    next();
+  }
+};
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+    if (existing) {
+      next({ status: 400, message: 'that name is taken'})
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      next({ status: 404, message: "not found" });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,79 +1,81 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const Account = require('../accounts/accounts-model')
+const middlewares = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+const Account = require("../accounts/accounts-model");
+
+router.get("/", (req, res, next) => {
   Account.getAll()
-    .then((accounts) =>{
-      if (!accounts){
+    .then((accounts) => {
+      if (!accounts) {
         res.status(200).json([]);
       } else {
         res.status(200).json(accounts);
       }
     })
     .catch(next);
-})
-
-router.get('/:id', (req, res, next) => {
-  Account.getById(req.params.id)
-    .then((account) => {
-      if (!account) {
-        res.status(404).json({
-          message: "The account with the specified ID does not exist",
-        });
-      } else {
-        res.status(200).json(account);
-      }
-    })
-    .catch(next);
-})
-
-router.post('/', (req, res, next) => {
-  const { name, budget } = req.body;
-  if (!name && !budget) {
-    res.status(400).json({
-      message: "Please provide some info for the account",
-    });
-  } else {
-    Account.create({ name, budget })
-      .then((newAcc) => {
-        res.status(201).json(newAcc);
-      })
-      .catch(next);
-  }
-})
-
-router.put('/:id', (req, res, next) => {
-  const { name, budget } = req.body;
-  if (!name && !budget) {
-    res.status(400).json({
-      message:
-        "Please provide some info for the project",
-    });
-  } else {
-    Account.getById(req.params.id)
-      .then((project) => {
-        if (!project) {
-          res.status(404).json({
-            message: "The acc with the specified ID does not exist",
-          });
-        } else {
-          return Account.updateById(req.params.id, req.body);
-        }
-      })
-      .then((info) => {
-        if (info) {
-          return Account.getById(req.params.id);
-        }
-      })
-      .then((acc) => {
-        res.status(201).json(acc);
-      })
-      .catch(next);
-  }
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.get("/:id", middlewares.checkAccountId, async (req, res, next) => {
+  res.json(req.account);
+});
+
+router.post(
+  "/",
+  middlewares.checkAccountPayload,
+  middlewares.checkAccountNameUnique,
+  (req, res, next) => {
+    const { name, budget } = req.body;
+    if (!name && !budget) {
+      res.status(400).json({
+        message: "Please provide some info for the account",
+      });
+    } else {
+      Account.create({ name, budget })
+        .then((newAcc) => {
+          res.status(201).json(newAcc);
+        })
+        .catch(next);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  middlewares.checkAccountId,
+  middlewares.checkAccountNameUnique,
+  middlewares.checkAccountPayload,
+  (req, res, next) => {
+    const { name, budget } = req.body;
+    if (!name && !budget) {
+      res.status(400).json({
+        message: "Please provide some info for the project",
+      });
+    } else {
+      Account.getById(req.params.id)
+        .then((project) => {
+          if (!project) {
+            res.status(404).json({
+              message: "The acc with the specified ID does not exist",
+            });
+          } else {
+            return Account.updateById(req.params.id, req.body);
+          }
+        })
+        .then((info) => {
+          if (info) {
+            return Account.getById(req.params.id);
+          }
+        })
+        .then((acc) => {
+          res.status(201).json(acc);
+        })
+        .catch(next);
+    }
+  }
+);
+
+router.delete("/:id", middlewares.checkAccountId, async (req, res, next) => {
   try {
     const acc = await Account.getById(req.params.id);
     if (!acc) {
@@ -89,12 +91,13 @@ router.delete('/:id', async (req, res, next) => {
   }
 });
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     custom: "Info could not be retrieved!",
     message: err.message,
     stack: err.stack,
   });
-})
+});
 
 module.exports = router;
